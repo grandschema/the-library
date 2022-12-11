54022f3c7861565d0cf6bbf3b541d1d187c09998
Jason Delgado <blackhat23507@gmail.com>
2022-12-11 11:22:25 -0600

    Dynamically add card to homepage

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+const bookCardsSection = document.querySelector(".book-cards-section");
+
+// Dynamically create card element
+{/* <div class="card" style="width: 18rem;" data-attr="1">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+    </div>
+</div> */}
+function addCard() {
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.setAttribute("style", "width: 18rem");
+    card.setAttribute("data-attr", "1");
+    card.innerHTML = `<div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+</div>`;
+    bookCardsSection.appendChild(card);
+}
 
 let submitBtn = document.querySelector("button[type='submit']");
 submitBtn.addEventListener('click', function(event){
@@ -33,5 +59,6 @@ function displayBooks() {
     });
 }
 
+addCard();
 // addBookToLibrary();
 // displayBooks();
