f2848e3bf60a4c24afd8f746942c317fb36286ed
Jason Delgado <blackhat23507@gmail.com>
2022-12-11 12:45:13 -0600

    Fix issue with reiterating the array of books and displaying multiples of the same book

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
 const bookCardsSection = document.querySelector(".book-cards-section");
-
-// Dynamically create card element
-{/* <div class="card" style="width: 18rem;" data-attr="1">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-    </div>
-</div> */}
-function addCard() {
-    let card = document.createElement("div");
-    card.setAttribute("class", "card");
-    card.setAttribute("style", "width: 18rem");
-    card.setAttribute("data-attr", "1");
-    card.innerHTML = `<div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-</div>`;
-    bookCardsSection.appendChild(card);
-}
+let index = 0;
 
 let submitBtn = document.querySelector("button[type='submit']");
 submitBtn.addEventListener('click', function(event){
@@ -31,8 +7,14 @@ submitBtn.addEventListener('click', function(event){
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
+
+    // Clear Input Fields
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
     addBookToLibrary(title, author, pages);
-    displayBooks();
+    displayBooks(index);
+    index += 1;
 });
 
 let myLibrary = [];
@@ -44,21 +26,30 @@ function Book(title, author, pgs) {
     this.hasRead = false;
 }
 
+// Dynamically create card element
+function addCard(title, author, pages, id) {
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.setAttribute("id", id);
+    card.setAttribute("style", "width: 18rem");
+    card.setAttribute("data-attr", "1");
+    card.innerHTML = `<div class="card-body">
+    <h5 class="card-title">${title}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Author: ${author}</h6>
+    <p class="pages">Pages: ${pages}</p>
+</div>`;
+    bookCardsSection.appendChild(card);
+}
+
 // title, author, pages, have you read it?, and a submit button
 function addBookToLibrary(t, a, p){
     let newBook = new Book(t, a, p);
     myLibrary.push(newBook);
 };
 
-function displayBooks() {
-    myLibrary.forEach(book => {
-        console.log(book.title);
-        console.log(book.author);
-        console.log(book.pages);
-        console.log(book.hasRead);
-    });
+function displayBooks(index) {
+    addCard(myLibrary[index].title, myLibrary[index].author, myLibrary[index].pages, index);
 }
 
-addCard();
 // addBookToLibrary();
 // displayBooks();
